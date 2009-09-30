72417d0ed8da24d8816e0b9c938c7d6179598ea8
davidkazuhiro <davidkazuhiro@1ff50526-aa1c-11de-9787-55e617d4dd53>
2009-09-30 03:52:33 +0000

    First commitsvn add books/ __init__.py manage.py settings.py.default urls.py ! Added skeleton django structure with some basic models

---
+++ trunk/books/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    barcode = models.CharField(max_length=50)
+    edition = models.IntegerField()
+    add_date = models.DateTimeField('Date Added')
+    # Should we have a course table? 
+    # TODO check to see how the old cube does courses
+
+class Student(models.Model):
+    number = models.CharField(max_length=10)
+
+class Listing(models.Model):
+    book = models.ForeignKey(Book)
+    list_date = models.DateTimeField('Date Listed')
+    seller = models.ForeignKey(Student, related_name="sell_item")
+    sell_date = models.DateTimeField('Date Sold')
+    holder = models.ForeignKey(Student, related_name="purchase_item")
+    hold_date = models.DateTimeField('Date Held')
