f337ae1cb37bff74f706129171ac60b4170293f6
bkotjan@gmail.com <bkotjan@gmail.com@1ff50526-aa1c-11de-9787-55e617d4dd53>
2009-11-19 21:52:44 +0000

    Began creating view for mybooks listing

--- trunk/cube/books/admin.py
+++ trunk/cube/books/admin.py
@@ -1,6 +1,6 @@
-from cube.books.models import Student, Book, Listing
+from cube.books.models import Book, Listing
 from django.contrib import admin
 
 admin.site.register(Book)
 admin.site.register(Listing)
-admin.site.register(Student)
+
--- trunk/cube/books/views.py
+++ trunk/cube/books/views.py
@@ -1,1 +1,18 @@
-# Add views if need be
+from cube.books.models import Book
+from cube.books.models import Listing
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+def myBooksies(request):
+    if request.user.is_authenticated():
+        #john = request.user.last_name
+        
+        work = Listing.objects.filter(seller = request.user)
+        me = request.user
+	 
+        #need title, author, price, course code, ref#
+        return HttpResponse(me)    
+    else:
+        return HttpResponse("No work")
+
+
