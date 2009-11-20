41be7402ba1810cdfee6e29aee6d249047feab83
davidkazuhiro <davidkazuhiro@1ff50526-aa1c-11de-9787-55e617d4dd53>
2009-11-20 06:56:47 +0000

    Enabled twupass. twupass is a separate module which we won't publish. this change may cause some trouble for the other guys, they'll have to update their settings and install the twupass module

--- cube/books/views.py
+++ cube/books/views.py
@@ -1,7 +1,20 @@
-from cube.books.models import Book
-from cube.books.models import Listing
+from cube.decorators import login_required
+from cube.books.models import Book, Listing
+from django.views.generic.list_detail import object_list
+from django.http import HttpResponseRedirect, HttpResponse
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+# http://docs.djangoproject.com/en/dev/topics/auth/#handling-authorization-in-custom-backends
+
+@login_required()
+def listings(request):
+    #if not request.user.is_authenticated():
+    #    return HttpResponseRedirect(LOGIN_URL)
+    #return object_list(*args, **kwargs)
+    listings = Listing.objects.all()
+    return render_to_response('books/listing_list.html', {'listings': listings},
+                              context_instance=RequestContext(request))
 
 def myBooksies(request):
     if request.user.is_authenticated():
@@ -14,5 +27,3 @@ def myBooksies(request):
         return HttpResponse(me)    
     else:
         return HttpResponse("No work")
-
-
