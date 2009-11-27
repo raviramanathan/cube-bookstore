47142f997918b4c50c0eae5a111a7db4ff90687d
davidkazuhiro <davidkazuhiro@1ff50526-aa1c-11de-9787-55e617d4dd53>
2009-11-27 01:20:19 +0000

    added javascript checkbox toggling to listing page

---
+++ media/js/cube.js
@@ -0,0 +1,31 @@
+function toggleCBTogether()
+{
+    var inputlist = document.getElementsByTagName("input");
+    checkboxCount = 0;
+    checked = 0;
+    for (i = 0; i < inputlist.length; i++)
+    {
+        if (inputlist[i].getAttribute("type") == 'checkbox')
+        {
+            checkboxCount++;
+            if (inputlist[i].checked)
+                checked++;
+        }
+    }
+    if (checked > checkboxCount / 2)
+        setCheckboxes(false);
+    else
+        setCheckboxes(true);
+}
+
+
+
+function setCheckboxes(set) 
+{
+    var inputlist = document.getElementsByTagName("input");
+    for (i = 0; i < inputlist.length; i++) 
+    {
+        if ( inputlist[i].getAttribute("type") == 'checkbox' ) 
+            inputlist[i].checked = set;
+    }
+}
