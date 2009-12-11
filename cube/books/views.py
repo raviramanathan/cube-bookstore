68ace913489793a665f3f6a0558aa2c73264e048
davidkazuhiro <davidkazuhiro@1ff50526-aa1c-11de-9787-55e617d4dd53>
2009-12-11 05:30:07 +0000

    Delete and Missing work now. I think Place on Hold and Remove Holds works too... I still need to do Sold, Seller Paid, Edit, and To Be Deleted

--- cube/books/views.py
+++ cube/books/views.py
@@ -1,5 +1,7 @@
+# django imports
 from cube.books.models import Book, Listing
 from cube.books.view_tools import listing_filter, listing_sort, get_number
+from cube.books.email import send_missing_emails
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.http import HttpResponseRedirect, HttpResponse
@@ -7,6 +9,9 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.contrib.auth.models import User
+
+#python imports
+from datetime import datetime
 
 # pagination defaults
 PER_PAGE = '30'
@@ -58,50 +63,99 @@ def update_data(request):
         if number == 1: return "1 item has been"
         return "%s items have been" % number
 
-    def set_statuses(status, listings):
+    def set_bunch(attr, value):
         """
         Sets a status on all listings given and saves them
         """
-        for listing in listings:
-            listing.status = status
+        for listing in bunch:
+            setattr(listing, attr, value)
             listing.save()
 
-    edit = []
+    messages = []
+    bunch = []
     action = request.POST.get("Action", '')
     singular = "item has been"
     plural = "items have been"
 
     for key, value in request.POST.items():
         if "idToEdit" in key:
-            edit.append(Listing.objects.get(pk=int(value)))
+            bunch.append(Listing.objects.get(pk=int(value)))
             
     if action == "Delete":
-        set_statuses('D', edit)
-        message = "%s deleted." % singlural(len(edit))
+        set_bunch('status', 'D')
+        messages.append("%s deleted." % singlural(len(bunch)))
     elif action[:1] == "To Be Deleted"[:1]:
         # apparently some browsers have issues passing spaces
         # TODO add bells and whistles
-        set_statuses('T', edit)
-        message = "%s marked as 'To Be Deleted'." % singlural(len(edit))
+        set_bunch('status', 'T')
+        messages.append("%s marked as 'To Be Deleted'." % singlural(len(bunch)))
     elif action == "Sold":
-        set_statuses('S', edit)
+        set_bunch('status', 'S')
         #TODO implement email and the bells and whistles
-        message = "%s set to Sold and the owners have been emailed " +\
-                  "and asked to come pickup the money." % singlural(len(edit))
+        messages.append("%s set to Sold and the owners " %\
+                        singlural(len(bunch)) +\
+                        "have been emailed and asked to come pickup the money.")
     elif action[:5] == "Seller Paid"[:5]:
         # apparently some browsers have issues passing spaces
         # TODO add the bells and whistles
-        set_statuses('P', edit)
-        message = "%s set to Seller Paid" % singlural(len(edit))
+        set_bunch('status', 'P')
+        messages.append("%s set to Seller Paid." % singlural(len(bunch)))
+    elif action == "Missing":
+        # only non-missing ones can be set to missing
+        bunch = filter(lambda x : x.status != 'M', bunch)
+        set_bunch('status', 'M')
+        send_missing_emails(bunch)
+        owners = set(map(lambda x: x.seller, bunch)) # unique owners
+        messages.append("%s set to Missing " % singlural(len(bunch)) +\
+                        "" if len(bunch) == 0 else\
+                        ("and the owner%s been notified via email." %\
+                        (" has" if len(owners) == 1 else "s have")))
     elif action[:4] == "Place on Hold"[:4]:
         # apparently some browsers have issues passing spaces
-        # TODO add the bells and whistles
-        set_statuses('O', edit)
-        message = "POH Under Construction..."
-    #elif action == "Missing":
-    message = "Under Construction..."
+        def hold_filter(x):
+            """
+            Only let listings which are For Sale or already On Hold
+            be Placed on Hold
+            """
+            if x.get_status_display() == 'On Hold' and x.holder == request.user:
+                messages.append('"%s" has been placed ' % x.book.title +\
+                                "on hold for another 24 hours. $%s" % x.price)
+                return x
+            elif x.get_status_display() != 'For Sale':
+                messages.append('"%s"' % x.book.title +\
+                                'was marked %s ' % x.get_status_display() +\
+                                "just before you requested it.")
+            else:
+                messages.append('"%s" has been reserved ' % x.book.title +\
+                                "for you for 24 hours. $%s" % x.price)
+                return x
+
+        bunch = filter(hold_filter, bunch)
+        set_bunch('status', 'O')
+        set_bunch('hold_date', datetime.today())
+        messages.append("Total: $%s" % sum(map(lambda x: x.price, bunch)))
+        messages.append("Please pickup your Requested Books within the next" +\
+                        ' 24 hours from the "Cube" which is by the cafeteria.')
+    elif action[:5] == "Remove Holds"[:5]:
+        def rm_hold_filter(x):
+            """ 
+            Filters listings through if the current user is staff
+            or the holder is the current user and the status is 'On Hold'
+            """
+            if x.status == 'O':
+                if request.user.is_staff: return x
+                if x.holder == request.user: return x
+        
+        bunch = filter(rm_hold_filter, bunch)
+        set_bunch('status', 'F') # set to "For Sale"
+        set_bunch('hold_date', None)
+        messages.append("%s hold%s been removed." % (len(bunch),
+            ' has' if len(bunch) == 1 else 's have'))
+    else:
+        # TODO edit
+        messages.append("Something went wrong... talk to whoever is in charge")
     vars = {
-        'message' : message, 
+        'messages' : messages, 
     }
     return render_to_response('books/listing_edit.html', vars, 
                               context_instance=RequestContext(request))
