e5a3715c4b3620fc8f2bec4151294f8914f07616
davidkazuhiro <davidkazuhiro@1ff50526-aa1c-11de-9787-55e617d4dd53>
2010-01-07 05:40:17 +0000

    logs work now and book editing is much improved but by no means complete

--- cube/books/views.py
+++ cube/books/views.py
@@ -76,6 +76,8 @@ def update_listing(request):
             
     if action == "Delete":
         bunch = bunch.exclude(status='D')
+        for listing in bunch:
+            Log(action='D', listing=listing, who=request.user).save()
         vars = { 'num_deleted': bunch.count() }
         bunch.update(status='D')
         return render_to_response('books/update_listing/deleted.html', vars,
@@ -86,6 +88,8 @@ def update_listing(request):
         # can't do this for Deleted, Seller Paid, and Sold Books
         bunch = bunch.exclude(status__in='DPS')
         send_tbd_emails(bunch)
+        for listing in bunch:
+            Log(action='T', listing=listing, who=request.user).save()
         vars = {
             'num_doomed' : bunch.count(),
             'num_owners' : len(set(map(lambda x: x.seller, bunch))),
@@ -96,6 +100,8 @@ def update_listing(request):
     elif action == "Sold":
         # Allow only if For Sale or On Hold
         bunch = bunch.filter(status__in='FO')
+        for listing in bunch:
+            Log(action='S', listing=listing, who=request.user).save()
         send_sold_emails(list(bunch))
         vars = {
             'sold' : bunch.count(),
@@ -106,12 +112,13 @@ def update_listing(request):
                                   context_instance=RequestContext(request))
     elif action[:5] == "Seller Paid"[:5]:
         # apparently some browsers have issues passing spaces
-
         # only staff can do this
         if not request.user.is_staff: bunch = Listing.objects.none()
         # A Seller can be paid only after the book was sold
         else: bunch = bunch.filter(status='S')
 
+        for listing in bunch:
+            Log(action='P', listing=listing, who=request.user).save()
         vars = {'paid' : bunch.count()}
         bunch.update(status='P')
         return render_to_response('books/update_listing/seller_paid.html', vars, 
@@ -119,6 +126,8 @@ def update_listing(request):
     elif action == "Missing":
         # Must be For Sale, On Hold or To Be Deleted for it to go Missing
         bunch = bunch.filter(status__in='FOT')
+        for listing in bunch:
+            Log(action='M', listing=listing, who=request.user).save()
         send_missing_emails(bunch)
         vars = {
             'num_owners' : len(set(map(lambda x: x.seller, bunch))),
@@ -132,6 +141,10 @@ def update_listing(request):
         extended = bunch.filter(status='O', holder=request.user)
         new_hold = bunch.filter(status='F')
         failed = bunch.exclude(status__in='OF', holder=request.user)
+        for listing in new_hold:
+            Log(action='O', listing=listing, who=request.user).save()
+        for listing in extended:
+            Log(action='X', listing=listing, who=request.user).save()
         held = extended | new_hold
         vars = {
             'failed' : failed,
@@ -147,6 +160,8 @@ def update_listing(request):
     elif action[:5] == "Remove Holds"[:5]:
         bunch = bunch.filter(status='O')
         if not request.user.is_staff: bunch = bunch.filter(holder=request.user)
+        for listing in bunch:
+            Log(action='R', listing=listing, who=request.user).save()
         vars = {'removed' : bunch.count()}
         bunch.update(status='F', hold_date=None, holder=None)
         return render_to_response('books/update_listing/remove_holds.html', vars,
@@ -154,14 +169,15 @@ def update_listing(request):
     elif action == "Edit":
         too_many = True if bunch.count() > 1 else False
         item = bunch[0]
-        rows = [{'name' : 'Bar Code', 'value' : item.book.barcode},
-                {'name' : 'Student ID', 'value' : item.seller.id},
+        rows = [{'name' : 'Seller Student ID', 'value' : item.seller.id},
                 {'name' : 'Price', 'value' : item.price},
-                {'name' : 'Buyer Student ID', 'value' : item.holder.id if item.holder else ''}]
+                {'name' : 'Bar Code', 'value' : item.book.barcode}]
+        logs = Log.objects.filter(listing=item)
         vars = {
             'rows' : rows,
             'too_many' : too_many,
             'id' : item.id,
+            'logs' : logs,
         }
         return render_to_response('books/update_listing/edit.html', vars, 
                                   context_instance=RequestContext(request))
@@ -169,6 +185,55 @@ def update_listing(request):
         vars = {'action' : action}
         return render_to_response('books/update_listing/error.html', vars, 
                                   context_instance=RequestContext(request))
+@login_required()
+def update_listing_edit(request):
+    """
+    Applies changes to a listing made on the edit page
+    If the barcode doesn't exist,
+    it makes the user create a Book object as well
+    """
+    if request.POST.has_key('IdToEdit'):
+        listing = Listing.objects.get(id=int(request.POST["IdToEdit"]))
+        g = request.POST.get
+        if g('bar-code', ''):
+            # there is a barcode
+            barcode = g('bar-code', '')
+            if Book.objects.filter(barcode=barcode):
+                # a book exists in the db with that barcode
+                listing.book = Book.objects.get(barcode=barcode)
+            else:
+                # barcode doesn't exist in db, we have to make a book.
+                vars = {
+                    'barcode' : barcode,
+                    'student_id' : g('seller-student-id', str(listing.seller.id)),
+                    'price' : g('price', str(listing.price)),
+                    'edition_range' : range(1,51),
+                    'hidden_fields' : {
+                        'listing_id' : listing.id,
+                    },
+                }
+                return render_to_response('books/new_book.html', vars,
+                                          context_instance=RequestContext(request))
+        else:
+            message = "There was no barcode"
+            return render_to_response('error.html', {'message' : message },
+                                  context_instance=RequestContext(request))
+        # TODO finish this
+        listing.book = Book.objects.get(barcode=g('bar-code', listing.book.barcode))
+        listing.seller = User.objects.get(id=int(g('seller-student-id', str(listing.seller.id))))
+        listing.price = Decimal(g('price', str(listing.price)))
+        listing.save()
+        Log(who=request.user, action='E', listing=listing).save()
+        return HttpResponse("It should have worked")
+    else:
+        message = "There was no IdToEdit"
+        return render_to_response('error.html', {'message' : message },
+                              context_instance=RequestContext(request))
+
+@login_required()
+def attach_book(request):
+    return HttpResponse("Now is the time when I should attach the book to the given listing...")
+
 @login_required()
 def myBooksies(request):
     """
@@ -284,7 +349,7 @@ def update_staff(request):
                 user.is_staff = True
                 user.save()
             vars = {
-                'user_name' : "%s %s" % (user.first_name, user.last_name),
+                'user_name' : user.get_full_name(),
                 'administrator' : user.is_superuser,
             }
             return render_to_response('books/update_staff/saved.html', vars, 
@@ -317,8 +382,7 @@ def staffedit(request):
     vars = {
         'edit' : edit,
         'too_many' : len(users) > 1,
-        'first_name' : users[0].first_name,
-        'last_name' : users[0].last_name,
+        'name' : users[0].get_full_name(),
         'student_id' : users[0].id,
         'current_role' : 'admin' if users[0].is_superuser else 'staff',
     }
@@ -326,7 +390,7 @@ def staffedit(request):
                               context_instance=RequestContext(request))
 
 @login_required()
-def add_books(request):
+def add_book(request):
     if request.POST.get("student_id") and request.POST.get("Price") and request.POST.get("BarCode"):
         barcode = request.POST.get("BarCode")
         student_id = int(request.POST.get("student_id"))
@@ -339,8 +403,9 @@ def add_books(request):
                 'student_id' : student_id,
                 'price' : price,
                 'edition_range' : range(1,51),
+                'hidden_fields' : {},
             }
-            return render_to_response('newBook.html', vars,
+            return render_to_response('books/new_book.html', vars,
                                       context_instance=RequestContext(request))
         try:
             seller = User.objects.get(id=student_id)
@@ -355,6 +420,7 @@ def add_books(request):
         listing = Listing(list_date=datetime.now(), price=price, status="F",
                           book=book, seller=seller)
         listing.save()
+        Log(listing=listing, who=request.user, action="C").save()
         vars = {
             'title' : book.title,
             'listing_id' : listing.id
@@ -362,7 +428,7 @@ def add_books(request):
         return render_to_response('books/update_listing/added.html', vars, 
                                   context_instance=RequestContext(request))
     elif request.POST.get("Action", '') == 'Add':
-        # This came from the add_books view, and we need to
+        # This came from the add_book view, and we need to
         # create a book and a listing
         g = lambda x: request.POST.get(x, '')
         barcode, price, sid, author, title, ed, dept, course_num =\
@@ -386,11 +452,12 @@ def add_books(request):
         listing = Listing(seller=seller, price=Decimal(price), book=book)
         listing.status = 'F'
         listing.save()
+        Log(listing=listing, who=request.user, action='A').save()
 
         vars = {
             'title' : book.title,
             'author' : book.author,
-            'seller_name' : "%s %s" % (seller.first_name, seller.last_name),
+            'seller_name' : seller.get_full_name()
         }
         return render_to_response('books/update_book/added.html', vars, 
                                   context_instance=RequestContext(request))
@@ -406,7 +473,7 @@ def add_books(request):
         return HttpResponse("happy")
     else:
         # the user is hitting the page for the first time
-        return render_to_response('books/add_books.html',
+        return render_to_response('books/add_book.html',
                                   context_instance=RequestContext(request))
 
 @login_required()
@@ -463,7 +530,6 @@ def update_books(request):
     #    rows = [{'name' : 'Bar Code', 'value' : item.book.barcode},
     #            {'name' : 'Student ID', 'value' : item.seller.id},
     #            {'name' : 'Price', 'value' : item.price},
-    #            {'name' : 'Buyer Student ID', 'value' : item.holder.id}]
     #    vars = {
     #        'rows' : rows,
     #        'too_many' : too_many,
