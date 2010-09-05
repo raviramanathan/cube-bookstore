382359447e8c10253db41fa890476081e2442afb
davidkazuhiro <davidkazuhiro@1ff50526-aa1c-11de-9787-55e617d4dd53>
2010-09-05 05:27:08 +0000

    Added report for books sold within a date range

--- cube/books/views/reports.py
+++ cube/books/views/reports.py
@@ -1,14 +1,19 @@
 # Copyright (C) 2010  Trinity Western University
 
 from cube.books.models import Book
+from cube.books.forms import DateRangeForm
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response as rtr
 from django.template import RequestContext as RC
+from django.http import HttpResponseNotAllowed
 
 @login_required()
 def menu(request):
-    return rtr('books/reports/menu.html', {}, context_instance=RC(request))
+    vars = {
+	'date_range_form': DateRangeForm(),
+    }
+    return rtr('books/reports/menu.html', vars, context_instance=RC(request))
 
 @login_required
 def per_status(request):
@@ -23,3 +28,27 @@ def per_status(request):
 	"deleted" : Book.objects.filter(status='D').count(),
     }
     return rtr('books/reports/per_status.html', vars, context_instance=RC(request))
+
+@login_required
+def books_sold_within_date(request):
+    """ Shows a list of all books sold within a given date range """
+    # TODO Untested!!
+    if request.method != "POST": return HttpResponseNotAllowed(['GET'])
+    date_range_form = DateRangeForm(request.POST)
+    if not date_range_form.is_valid():
+        # TODO Untested!
+	vars = {
+	    'date_range_form' : date_range_form,
+	}
+	return rtr('books/reports/menu.html', vars, context_instance=RC(request))
+	
+    to_date = date_range_form.cleaned_data['to_date']
+    from_date = date_range_form.cleaned_data['from_date']
+    sold_books = Book.objects.filter(status='S', sell_date__gte=from_date).exclude(sell_date__gt=to_date)
+    vars = {
+	'count' : sold_books.count(),
+	'sold_books' : sold_books,
+	'from_date' : from_date,
+	'to_date' : to_date,
+    }
+    return rtr('books/reports/books_sold_within_date.html', vars, context_instance=RC(request))
